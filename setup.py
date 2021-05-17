9a9a91473bdb1f357bf40cd513fec4b5793c6fc0
David Cain <david.cain@color.com>
2021-05-17 15:35:43 -0700

    Format `setup.py` consistently
    This formatting is just an invocation of `black`, but should be fairly
    standard formatting for pretty much any tool.
    
    Note that `black` is not actually configured for this repository, but
    that's okay.

--- setup.py
+++ setup.py
@@ -4,25 +4,26 @@ except ImportError:
     from distutils.core import setup
 
 
-setup(name = "clrsvsim",
-      version = "0.1.0",
-      description = "Color Genomics Structural Variant Simulator",
-      author = "Color Genomics",
-      author_email = "dev@color.com",
-      url = "https://github.com/ColorGenomics/clrsvsim",
-      packages = ["clrsvsim"],
-      install_requires=[
-        'cigar>=0.1.3',
-        'numpy>=1.10.1',
-        'preconditions>=0.1',
-        'pyfasta>=0.5.2',
-        'pysam>=0.10.0',
-      ],
-      tests_require=[
+setup(
+    name="clrsvsim",
+    version="0.1.0",
+    description="Color Genomics Structural Variant Simulator",
+    author="Color Genomics",
+    author_email="dev@color.com",
+    url="https://github.com/ColorGenomics/clrsvsim",
+    packages=["clrsvsim"],
+    install_requires=[
+        "cigar>=0.1.3",
+        "numpy>=1.10.1",
+        "preconditions>=0.1",
+        "pyfasta>=0.5.2",
+        "pysam>=0.10.0",
+    ],
+    tests_require=[
         # NOTE: `mock` is not actually needed in Python 3.
         # `unittest.mock` can be used instead.
-        'mock>=2.0.0',
-        'nose>=1.3.7',
-      ],
-      license = "Apache-2.0",
+        "mock>=2.0.0",
+        "nose>=1.3.7",
+    ],
+    license="Apache-2.0",
 )
